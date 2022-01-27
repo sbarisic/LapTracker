0688a9f606aa20e979cdcaf8dd72be48e22d53cc
Saša Barišić <sbarisic@vub.hr>
2022-01-27 22:29:38 +0100

    Added osmdroid map

--- app/src/main/java/hr/vub/laptracker/FirstFragment.java
+++ app/src/main/java/hr/vub/laptracker/FirstFragment.java
@@ -9,6 +9,15 @@ import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
 import androidx.navigation.fragment.NavHostFragment;
 
+import org.osmdroid.api.IMapController;
+import org.osmdroid.tileprovider.tilesource.TileSourceFactory;
+import org.osmdroid.util.GeoPoint;
+import org.osmdroid.views.MapView;
+import org.osmdroid.views.overlay.compass.CompassOverlay;
+import org.osmdroid.views.overlay.compass.InternalCompassOrientationProvider;
+import org.osmdroid.views.overlay.mylocation.GpsMyLocationProvider;
+import org.osmdroid.views.overlay.mylocation.MyLocationNewOverlay;
+
 import hr.vub.laptracker.databinding.FragmentFirstBinding;
 
 public class FirstFragment extends Fragment {
@@ -17,20 +26,40 @@ public class FirstFragment extends Fragment {
 
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
-
         binding = FragmentFirstBinding.inflate(inflater, container, false);
-        return binding.getRoot();
 
+        MainActivity mainAct = (MainActivity) getActivity();
+
+
+        mainAct.map = binding.map;
+        mainAct.map.setTileSource(TileSourceFactory.MAPNIK);
+        mainAct.map.setBuiltInZoomControls(true);
+        mainAct.map.setMultiTouchControls(true);
+
+        IMapController mapController = mainAct.map.getController();
+        mapController.setZoom(15);
+        GeoPoint startPoint = new GeoPoint(45.89857767203944, 16.842182187700285);
+        mapController.setCenter(startPoint);
+
+        /*CompassOverlay mCompassOverlay = new CompassOverlay(mainAct.ctx, new InternalCompassOrientationProvider(mainAct.ctx), mainAct.map);
+        mCompassOverlay.enableCompass();
+        mainAct.map.getOverlays().add(mCompassOverlay);*/
+
+        MyLocationNewOverlay mLocationOverlay = new MyLocationNewOverlay(new GpsMyLocationProvider(mainAct.ctx), mainAct.map);
+        mLocationOverlay.enableMyLocation();
+        mainAct.map.getOverlays().add(mLocationOverlay);
+
+        return binding.getRoot();
     }
 
     public void onViewCreated(@NonNull View view, Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
 
-        binding.buttonFirst.setOnClickListener(view1 -> NavHostFragment.findNavController(FirstFragment.this)
+        /*binding.buttonFirst.setOnClickListener(view1 -> NavHostFragment.findNavController(FirstFragment.this)
                 .navigate(R.id.action_FirstFragment_to_SecondFragment));
 
         binding.buttonSettings.setOnClickListener(view12 -> NavHostFragment.findNavController(FirstFragment.this)
-                .navigate(R.id.action_FirstFragment_to_SettingsFragment));
+                .navigate(R.id.action_FirstFragment_to_SettingsFragment));*/
     }
 
     @Override
--- app/src/main/java/hr/vub/laptracker/MainActivity.java
+++ app/src/main/java/hr/vub/laptracker/MainActivity.java
@@ -1,9 +1,13 @@
 package hr.vub.laptracker;
 
+import android.Manifest;
+import android.content.Context;
 import android.os.Bundle;
 
 import com.google.android.material.snackbar.Snackbar;
 
+import androidx.activity.result.ActivityResultLauncher;
+import androidx.activity.result.contract.ActivityResultContracts;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.view.View;
@@ -12,20 +16,62 @@ import androidx.navigation.NavController;
 import androidx.navigation.Navigation;
 import androidx.navigation.ui.AppBarConfiguration;
 import androidx.navigation.ui.NavigationUI;
+import androidx.preference.PreferenceManager;
 
 import hr.vub.laptracker.databinding.ActivityMainBinding;
 
 import android.view.Menu;
 import android.view.MenuItem;
 
+import org.osmdroid.config.Configuration;
+import org.osmdroid.tileprovider.tilesource.TileSourceFactory;
+import org.osmdroid.views.MapView;
+
+
 public class MainActivity extends AppCompatActivity {
 
     private AppBarConfiguration appBarConfiguration;
     private ActivityMainBinding binding;
 
+    public Context ctx = null;
+    public MapView map = null;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+        //handle permissions first, before map is created. not depicted here
+        ActivityResultLauncher<String[]> locationPermissionRequest =
+                registerForActivityResult(new ActivityResultContracts
+                                .RequestMultiplePermissions(), result -> {
+                            Boolean fineLocationGranted = result.getOrDefault(
+                                    Manifest.permission.ACCESS_FINE_LOCATION, false);
+                            Boolean coarseLocationGranted = result.getOrDefault(
+                                    Manifest.permission.ACCESS_COARSE_LOCATION,false);
+                            if (fineLocationGranted != null && fineLocationGranted) {
+                                // Precise location access granted.
+                            } else if (coarseLocationGranted != null && coarseLocationGranted) {
+                                // Only approximate location access granted.
+                            } else {
+                                // No location access granted.
+                            }
+                        }
+                );
+
+        locationPermissionRequest.launch(new String[] {
+                Manifest.permission.ACCESS_FINE_LOCATION,
+                Manifest.permission.ACCESS_COARSE_LOCATION
+        });
+
+
+        //load/initialize the osmdroid configuration, this can be done
+        ctx = getApplicationContext();
+        Configuration.getInstance().load(ctx, PreferenceManager.getDefaultSharedPreferences(ctx));
+        //setting this before the layout is inflated is a good idea
+        //it 'should' ensure that the map has a writable location for the map cache, even without permissions
+        //if no tiles are displayed, you can try overriding the cache path using Configuration.getInstance().setCachePath
+        //see also StorageUtils
+        //note, the load method also sets the HTTP User Agent to your application's package name, abusing osm's tile servers will get you banned based on this string
 
         binding = ActivityMainBinding.inflate(getLayoutInflater());
         setContentView(binding.getRoot());
@@ -35,6 +81,32 @@ public class MainActivity extends AppCompatActivity {
         NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment_content_main);
         appBarConfiguration = new AppBarConfiguration.Builder(navController.getGraph()).build();
         NavigationUI.setupActionBarWithNavController(this, navController, appBarConfiguration);
+    }
+
+    @Override
+    public void onResume() {
+        super.onResume();
+
+        if (map != null) {
+            //this will refresh the osmdroid configuration on resuming.
+            //if you make changes to the configuration, use
+            //SharedPreferences prefs = PreferenceManager.getDefaultSharedPreferences(this);
+            //Configuration.getInstance().load(this, PreferenceManager.getDefaultSharedPreferences(this));
+            map.onResume(); //needed for compass, my location overlays, v6.0.0 and up
+        }
+    }
+
+    @Override
+    public void onPause() {
+        super.onPause();
+
+        if (map != null) {
+            //this will refresh the osmdroid configuration on resuming.
+            //if you make changes to the configuration, use
+            //SharedPreferences prefs = PreferenceManager.getDefaultSharedPreferences(this);
+            //Configuration.getInstance().save(this, prefs);
+            map.onPause();  //needed for compass, my location overlays, v6.0.0 and up
+        }
     }
 
     @Override
